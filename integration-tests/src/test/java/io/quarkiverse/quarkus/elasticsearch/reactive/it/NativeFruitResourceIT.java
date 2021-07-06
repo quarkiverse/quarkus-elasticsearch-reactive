09f709355e9681d4ff530e04fbcbad0e01f3f48d
Loïc Mathieu <loikeseke@gmail.com>
2021-07-06 12:35:01 +0200

    First implementation

---
+++ integration-tests/src/test/java/io/quarkiverse/quarkus/elasticsearch/reactive/it/NativeFruitResourceIT.java
@@ -0,0 +1,9 @@
+package io.quarkiverse.quarkus.elasticsearch.reactive.it;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFruitResourceIT extends FruitResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ runtime/src/main/java/io/quarkiverse/quarkus/elasticsearch/reactive/runtime/ElasticsearchReactiveProducer.java
@@ -0,0 +1,28 @@
+package io.quarkiverse.quarkus.elasticsearch.reactive.runtime;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.elasticsearch.client.RestClientBuilder;
+
+import io.quarkus.elasticsearch.restclient.lowlevel.runtime.ElasticsearchConfig;
+import io.reactiverse.elasticsearch.client.mutiny.RestHighLevelClient;
+import io.vertx.mutiny.core.Vertx;
+
+@ApplicationScoped
+public class ElasticsearchReactiveProducer {
+    @Inject
+    ElasticsearchConfig config;
+
+    @Inject
+    Vertx vertx;
+
+    @Produces
+    @Singleton
+    public RestHighLevelClient restHighLevelClient() {
+        RestClientBuilder builder = RestClientBuilderHelper.createRestClientBuilder(config);
+        return RestHighLevelClient.create(vertx, builder);
+    }
+}
